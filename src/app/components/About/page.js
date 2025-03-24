9dbe996994ea6859e861feeba9d555aaa5313b33
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-03-24 14:37:36 +0600

    about me

--- src/app/components/About/page.js
+++ src/app/components/About/page.js
@@ -26,12 +26,54 @@ const About = (props) => {
 
 
                     {/* Right colums (text) */}
-                    
+                    <div className='lg:w-1/2 w-full'>
+                        <div className='text-center lg:text-left'>
+                            <small className='text-lg text-white'>About Me</small>
+                            <h2 className='text-3xl font-semibold mt-4 mb-2'>
+                                Welcome to Tonu, Best Software Services
+                            </h2>
+                            <h5 className='text-xl text-gray-700 mb-4 '>
+                             I have 8+ years of experience in Software Development to give you better services.
+
+                            </h5>
+                            <p className='text-gray-600 mb-6'>
+                                 A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence. 
+                            </p>
+
+
+                            {/* fun facts */}
+                            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 text-center'>
+                                <div className='funfact-item'>
+                                    <h3 className='text-4xl font-bold text-[#e5540a] '><span>500</span>+</h3>
+                                    <p className="text-lg text-gray-600">Projects Completed</p>
+
+
+                                </div>
+                                <div className="funfact-item">
+                                <h3 className="text-4xl font-bold text-blue-600"><span>52</span>+</h3>
+                                <p className="text-lg text-gray-600">Awards Win</p>
+                                </div>
+                                <div className="funfact-item">
+                                <h3 className="text-4xl font-bold text-blue-600"><span>2</span>M+</h3>
+                                <p className="text-lg text-gray-600">Happy Clients</p>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
 
                 </div>
 
             </div>
 
+        {/* visible text section */}
+        <div className='absolute top-1/2 left-0 w-full text-center'>
+            <h1 className='text-6xl font-bold text-white opacity-25 transform rotate-12'>About Me</h1>
+
+        </div>
         </section>
     );
 };
