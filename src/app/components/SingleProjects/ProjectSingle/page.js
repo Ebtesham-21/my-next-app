12e90010109f45cf985249ec7e2fc557d501fe3e
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-04-07 14:00:30 +0600

    project single

--- src/app/components/SingleProjects/ProjectSingle/page.js
+++ src/app/components/SingleProjects/ProjectSingle/page.js
@@ -43,6 +43,97 @@ const ProjectSingle = ({maxWidth, open, onClose, title, pImg, psub1img1, psub1im
 
 
                                                 </div>
+                                                <div className="w-full lg:w-5/12">
+                                                    <div className='tp-project-single-content-des-right'>
+                                                        <ul>
+                                                                <li>Location :<span>7 Lake Street,London</span></li>
+                                                                <li>Client :<span>wpOceans</span></li>
+                                                                <li>Consult :<span>Harry Johnson</span></li>
+                                                                <li>Project Type :<span>Tonu React</span></li>
+                                                                <li>Duration :<span>6 Month</span></li>
+                                                                <li>Completion :<span>15 Apr 2023</span></li>
+                                                                <li>Share :<span>Creative, Portfolio</span></li>
+                                                            </ul>
+
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+                                            <div className='tp-project-single-main-img'>
+                                                <Image src={pImg} alt=""/>
+
+                                            </div>
+
+                                        </div>
+                                        <div className='tp-project-single-item list-widget'>
+                                            <div className='flex flex-wrap'>
+                                                <div className='w-full lg:w-6/12'>
+                                                    <div className='tp-project-single-title'>
+                                                        <h3>Our Strategies</h3>
+
+                                                    </div>
+                                                    <p>
+                                                        Massa volutpat odio facilisis purus sit elementum. Non, sed velit dictum
+                                                        quam. Id
+                                                        risus pharetra est, at rhoncus, nec ullamcorper tincidunt. Id aliquet duis
+                                                        sollicitudin diam.
+
+                                                    </p>
+
+                                                    <ul>
+                                                        <li>Non saed velit dictum quam risus pharetra esta.</li>
+                                                        <li>Id risus pharetra est, at rhoncus, nec ullamcorper tincidunt.</li>
+                                                        <li>Hac nibh fermentum nisi, platea condimentum cursus.</li>
+                                                        <li>Massa volutpat odio facilisis purus sit elementum.</li>
+                                                        <li>Elit curabitur amet risus bibendum.</li>
+                                                    </ul>
+
+                                                </div>
+
+                                                <div className='w-full lg:w-6/12'>
+                                                    <div className='tp-project-single-item-quote'>
+                                                    <p>"Amazing looking theme and instantly turns your application into a great
+                                                            looking one. Really shows that pro_ fessionals built this theme up. Very
+                                                            happy with the way the theme looks ."</p>
+                                                        <span>Robert - <span>Yellow Theme</span></span>
+
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+
+
+
+                                        </div>
+                                        <div className='tp-project-single-item'>
+                                            <div className='tp-project-single-title'>
+                                                <h3>Our Approach</h3>
+
+                                            </div>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat suspendisse aenean
+                                                tellus augue morbi risus. Sit morbi vitae morbi sed urna sed purus. Orci facilisi
+                                                eros sed pellentesque. Risus id sed tortor sed scelerisque. Vestibulum elit
+                                                elementum, magna id viverra non, velit. Pretium, eros, porttitor fusce auctor vitae
+                                                id. Phasellus scelerisque nibh eleifend vel enim mauris purus. Rutrum vel sem
+                                                adipiscing nisi vulputate molestie scelerisque molestie ultrices. Eu, fusce
+                                                vulputate diam interdum morbi ac a.</p>
+
+
+                                        </div>
+                                        <div className='tp-project-single-gallery'>
+                                            <div className='flex flex-wrap mt-4'>
+                                                <div className="w-full sm:w-1/2 md:w-1/2">
+                                                    <div className='tp-p-details-img'>
+                                                        <Image src={psub1img1} alt=""/>
+
+                                                    </div>
+
+                                                </div>
+                                                <div className=''>
+
+                                                </div>
 
                                             </div>
 
